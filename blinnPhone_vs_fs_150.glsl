ef263dcd3c5656df4fdb12821002b161c75813fe
yangchuang <yangchuang@xingfeiinc.com>
2019-09-18 15:44:39 +0800

    修改shader

--- blinnPhone_vs_fs_150.glsl
+++ blinnPhone_vs_fs_150.glsl
@@ -61,7 +61,6 @@ uniform vec4 diffuseMat;   //材质属性
 uniform vec4 specularMat;  //反射光属性 RGBA --alpha表示高光强度 
 uniform vec4 ambientMat;   //漫反射材质属性
 uniform vec4 emissiveMat;  //伪自发光材质属性
-uniform float shiness;  //高光强度
 
 uniform int light_count;    //实际灯光数量
 uniform int light_type_array[MAX_LIGHT_COUNT];  //灯光类型 LT_POINT = 0, LT_DIRECTIONAL = 1, LT_SPOTLIGHT = 2
@@ -72,8 +71,7 @@ uniform vec3 light_position_array[MAX_LIGHT_COUNT];
   //Attenuation = Constant + Linear * Distance + Quadratic * Distance ^ 2
   //Luminosity = 1 / Attenuation
 uniform vec4 light_attenuation_array[MAX_LIGHT_COUNT]; //点光源，聚光灯衰减，平行光无效，
-uniform float spotlight_params_array[MAX_LIGHT_COUNT]; //聚光灯内半角cos值
-uniform int light_cast_shadow_array[MAX_LIGHT_COUNT]; //是否投射阴影
+uniform vec2 spotlight_params_array[MAX_LIGHT_COUNT]; //聚光灯外角cos值和内角cos值 outcutoff|cutoff
 
 
 uniform sampler2D diffuseMap;
@@ -82,36 +80,8 @@ uniform float minAlpha;
 uniform sampler2D normalMap;
 in vec4 oUv0;
 in vec4 oTangent;
-#else
-    #ifdef NORMAL_HEIGHTMAP
-    uniform sampler2D normalHeightMap;
-    uniform vec4 scaleBias;
-    in vec4 oUv0;
-    in vec4 oTangent;
-	vec2 steepPallaxMapping(in vec3 v, in vec2 t)
-	{
-		const float minLayers = 5;
-		const float maxLayers = 15;
-		float numLayers = mix(maxLayers, minLayers, abs(dot(vec3(0, 0, 1), v)));
-	 
-		float layerHeight = 8 / numLayers;
-		float currentLayerHeight = 0;
-		vec2 dtex = 0.6 * v.xy / v.z / numLayers;
-		vec2 currentTextureCoords = t;
-
-		float heightFromTexture = texture(normalHeightMap, currentTextureCoords).a;
-		while(heightFromTexture > currentLayerHeight) 
-		{
-			currentLayerHeight += layerHeight;
-			currentTextureCoords -= dtex;
-			heightFromTexture = texture(normalHeightMap, currentTextureCoords).a;
-		}
-	   return currentTextureCoords;
-	}
-    #else
-    in vec2 oUv0;
-    #endif
 #endif
+in vec2 oUv0;
 
 #ifdef SPECULAR_MAP
 uniform sampler2D specularMap;
@@ -121,7 +91,6 @@ uniform sampler2D aoMap;
 #endif
 
 uniform vec3 cameraPos;
-
 
 in vec3 fragPos;
 in vec3 oNormal;
@@ -136,17 +105,21 @@ uniform vec4 invShadowMapSize0;
 uniform vec4 invShadowMapSize1;
 uniform vec4 pssmSplitPoints;
 
-float shadowPCF(sampler2D shadowMap, vec4 shadowMapPos, vec2 offset)
+float shadowPCF(in sampler2D shadowMap, vec4 shadowMapPos, vec2 offset)
 {
 	shadowMapPos = shadowMapPos / shadowMapPos.w;
 	vec2 uv = shadowMapPos.xy;
-	vec3 o = vec3(offset, -offset.x) * 0.3f;
+	vec3 o = vec3(offset, -offset.x) * 0.49f;;
 
+   // float c = texture(shadowMap, uv.xy).r;
+	//float c =	(shadowMapPos.z <= texture(shadowMap, uv.xy).r) ? 1 : 0; // top left
+	
 	float c =	(shadowMapPos.z <= texture(shadowMap, uv.xy - o.xy).r) ? 1 : 0; // top left
 	c +=		(shadowMapPos.z <= texture(shadowMap, uv.xy + o.xy).r) ? 1 : 0; // bottom right
 	c +=		(shadowMapPos.z <= texture(shadowMap, uv.xy + o.zy).r) ? 1 : 0; // bottom left
 	c +=		(shadowMapPos.z <= texture(shadowMap, uv.xy - o.zy).r) ? 1 : 0; // top right
 	return c / 4;
+	//return c;
 }
 
 #endif
@@ -162,10 +135,8 @@ out vec4 FragColor;
 void main()
 {
 
-	#ifndef NORMAL_HEIGHTMAP
-		vec4 diffuseColor = texture(diffuseMap, oUv0.xy);
-		if(diffuseColor.a < minAlpha) {discard;}
-	#endif
+	vec4 diffuseColor = texture(diffuseMap, oUv0.xy);
+	if(diffuseColor.a < minAlpha) {discard;}
 
     vec3 viewDir = normalize(fragPos - cameraPos);
 	vec3 normal;
@@ -174,178 +145,155 @@ void main()
 		normal.x = dot(vec3(oTangent.x, oUv0.z, oNormal.x), tnormal);
 		normal.y = dot(vec3(oTangent.y, oUv0.w, oNormal.y), tnormal);
 		normal.z = dot(vec3(oTangent.z, oTangent.w, oNormal.z), tnormal);
-        normal = normalize(normal);	
-	#else
-        #ifdef NORMAL_HEIGHTMAP
-           // float height = texture2D(normalHeightMap, oUv0.xy).a;
-            //float displacement = (height * scaleBias.x) + scaleBias.y;
-            //vec3 uv2 = vec3(oUv0.xy, 1.0);
-          //  uv2.xy = ((viewDir * displacement) + uv2).xy;
-		  vec2 uv2 = steepPallaxMapping(viewDir, oUv0.xy);
-		  
-            vec3 tnormal = expand(texture2D(normalHeightMap, uv2.xy).xyz);
-	        normal.x = dot(vec3(oTangent.x, oUv0.z, oNormal.x), tnormal);
-	    	normal.y = dot(vec3(oTangent.y, oUv0.w, oNormal.y), tnormal);
-		    normal.z = dot(vec3(oTangent.z, oTangent.w, oNormal.z), tnormal);
-            normal = normalize(normal);	
-        #else
-		    normal = normalize(oNormal);
-        #endif
+        normal = normalize(normal);
+    #else
+        normal = normalize(oNormal);
 	#endif
+	 
+	#ifdef AO_MAP
+		vec4 aoColor = texture(aoMap, oUv0.xy);
+	#endif
+
+float shadowing = 1.0f;
+#ifdef RECEIVE_SHADOW_PSSM
+ if(light_type_array[0] == 1)  //平行光
+ {
+	vec3 meshNormal = normalize(oNormal);
+	vec3 lightdir = light_direction_array[0];
+	shadowing = abs(dot(meshNormal, lightdir));
+	#define SELFSHADOW_COS_MAX 0.01745240643728 //cos 89 degree
+	if ( shadowing > SELFSHADOW_COS_MAX )
+	{
+		if (zPosition <= pssmSplitPoints.y)
+		{
+			//splitColour = vec4(0.5, 0, 0, 1);
+			shadowing = shadowPCF(shadowMap0, oLightPosition0, invShadowMapSize0.xy);
+		}
+		else if(zPosition <= pssmSplitPoints.z)
+		{
+			shadowing = shadowPCF(shadowMap1, oLightPosition1, invShadowMapSize1.xy);
+		}
+		else
+		{
+			shadowing = 1.0f;
+		}
+	}
+	shadowing = shadowing + 0.35f;   //环境光0.35f， 阴影至少是环境光
+ } 
+#endif
 	
-	#ifdef NORMAL_HEIGHTMAP
-		vec4 diffuseColor = texture(diffuseMap, uv2.xy);
-		if(diffuseColor.a < minAlpha) {discard;}
-		#ifdef AO_MAP
-			vec4 aoColor = texture(aoMap, uv2.xy);
-		#endif
+	#ifdef AO_MAP
+		vec3 ambient = emissiveMat.rgb + AmbientColor.rgb * ambientMat.rgb * aoColor.rgb * diffuseColor.rgb;;
 	#else
-		#ifdef AO_MAP
-			vec4 aoColor = texture(aoMap, oUv0.xy);
-		#endif
+		vec3 ambient = emissiveMat.rgb + AmbientColor.rgb * ambientMat.rgb * diffuseColor.rgb;;
 	#endif
-		
-    vec3 result = vec3(0.0);
+    vec3 result = ambient;
     for(int i=0 ; i<light_count; i++)
     {
         if(light_type_array[i] == 1) //平行光
         {
-            #ifdef AO_MAP
-				vec3 ambient = AmbientColor.rgb * ambientMat.rgb * diffuseColor.rgb * aoColor.rgb;
-            #else
-				vec3 ambient = AmbientColor.rgb * ambientMat.rgb * diffuseColor.rgb;
-            #endif
-            vec3 diffuse =  light_diffuse_array[i].rgb * diffuseMat.rgb * max(dot(normalize(-light_direction_array[i]), normal), 0) * diffuseColor.rgb;
-            #ifdef LAMBERT
-                 result = result + ambient + diffuse;
-            #else
-                vec3 specular;
-                #ifdef BLINN_PHONE
-                    #ifdef SPECULAR_MAP
-						#ifdef NORMAL_HEIGHTMAP
-							specular = specularMat.rgb * pow(max(dot(normalize(normalize(-light_direction_array[i]) + viewDir), normal), 0), shiness) * vec3(texture(specularMap, uv2.xy));
+		
+			if(shadowing >= 1.0f)  //不在阴影中正常光照
+			{
+				shadowing = 1.0f;
+				
+				vec3 diffuse =  light_diffuse_array[i].rgb * diffuseMat.rgb * max(dot(normalize(-light_direction_array[i]), normal), 0);
+				#ifdef LAMBERT
+					 result = result + diffuse * diffuseColor.rgb;
+				#else
+					vec3 specular;
+					#ifdef BLINN_PHONE
+						#ifdef SPECULAR_MAP
+							specular = specularMat.rgb * pow(max(dot(normalize(normalize(-light_direction_array[i]) + viewDir), normal), 0), specularMat.a) * vec3(texture(specularMap, oUv0.xy));    
 						#else
-							specular = specularMat.rgb * pow(max(dot(normalize(normalize(-light_direction_array[i]) + viewDir), normal), 0), shiness) * vec3(texture(specularMap, oUv0.xy));
-						#endif      
-                    #else
-                        specular = specularMat.rgb * pow(max(dot(normalize(normalize(-light_direction_array[i]) + viewDir), normal), 0), shiness);
-                    #endif
-                #else
-                    #ifdef SPECULAR_MAP
-						#ifdef NORMAL_HEIGHTMAP
-							specular = specularMat.rgb * pow(max(dot(normalize(reflect(-light_direction_array[i], normal)), viewDir), 0),shiness) * vec3(texture(specularMap, uv2.xy));
+							specular = specularMat.rgb * pow(max(dot(normalize(normalize(-light_direction_array[i]) + viewDir), normal), 0), specularMat.a);
+						#endif
+					#else
+						#ifdef SPECULAR_MAP
+							specular = specularMat.rgb * pow(max(dot(normalize(reflect(-light_direction_array[i], normal)), viewDir), 0),specularMat.a) * vec3(texture(specularMap, oUv0.xy));                    
 						#else
-							specular = specularMat.rgb * pow(max(dot(normalize(reflect(-light_direction_array[i], normal)), viewDir), 0),shiness) * vec3(texture(specularMap, oUv0.xy));
-						#endif                     
-                    #else
-                        specular = specularMat.rgb * pow(max(dot(normalize(reflect(-light_direction_array[i], normal)), viewDir), 0), shiness);
-                    #endif
-                #endif
-                // result = result + ambient + diffuse + specular;
-                  //  result = result + diffuse + specular;
-		             result = result +ambient + diffuse+ specular;
-            #endif
+							specular = specularMat.rgb * pow(max(dot(normalize(reflect(-light_direction_array[i], normal)), viewDir), 0), specularMat.a);
+						#endif
+					#endif
+					 result = result + (diffuse + specular) * diffuseColor.rgb;
+					  //  result = result + diffuse + specular;
+						// result = result + ambient + diffuse;
+				#endif
+				
+			} 
+			else 
+			{
+				vec3 diffuse =  AmbientColor.rgb * diffuseMat.rgb * max(dot(normalize(-light_direction_array[i]), normal), 0);
+				result = result + diffuse * diffuseColor.rgb;
+				
+				//result = vec3(0.5,0.5,0.5) * diffuseColor.rgb;
+			}
         }
         else if(light_type_array[i] == 0) //点光源
         {  
-            vec3 light_dir = fragPos - light_position_array[i];
+            vec3 light_dir = light_position_array[i] - fragPos;
 			float dis = length(light_dir);
-            float attenuation = 1/(light_attenuation_array[i].r +light_attenuation_array[i].g * dis + light_attenuation_array[i].b * dis * dis); 
-            vec3 ambient = AmbientColor.rgb * ambientMat.rgb * diffuseColor.rgb;
-            vec3 diffuse = light_diffuse_array[i].rgb * diffuseMat.rgb * max(dot(normalize(light_dir), normal), 0) * diffuseColor.rgb;
+			light_dir = normalize(light_dir);
+            float attenuation = 1/(light_attenuation_array[i].g +light_attenuation_array[i].b * dis + light_attenuation_array[i].a * dis * dis); 
+            vec3 diffuse = light_diffuse_array[i].rgb * diffuseMat.rgb * max(dot(light_dir, normal), 0);
             #ifdef LAMBERT
-                 result = result + attenuation * (ambient + diffuse);
+                 result = result + attenuation * diffuse * diffuseColor.rgb;
             #else
                 vec3 specular;
                 #ifdef BLINN_PHONE
                     #ifdef SPECULAR_MAP
-						#ifdef NORMAL_HEIGHTMAP
-							specular = specularMat.rgb * pow(max(dot(normalize(normalize(-light_direction_array[i]) + viewDir), normal), 0), shiness) * vec3(texture(specularMap, uv2.xy));
-						#else
-							specular = specularMat.rgb * pow(max(dot(normalize(normalize(-light_direction_array[i]) + viewDir), normal), 0), shiness) * vec3(texture(specularMap, oUv0.xy));
-						#endif    
+						specular = specularMat.rgb * pow(max(dot(normalize(light_dir + viewDir), normal), 0), specularMat.a) * vec3(texture(specularMap, oUv0.xy));  
                     #else
-                        specular = specularMat.rgb * pow(max(dot(normalize(normalize(-light_dir) + normalize(viewDir)), normal), 0), specularMat.a);
+                        specular = specularMat.rgb * pow(max(dot(normalize(light_dir + normalize(viewDir)), normal), 0), specularMat.a);
                     #endif
                 #else
                     #ifdef SPECULAR_MAP
-						#ifdef NORMAL_HEIGHTMAP
-							specular = specularMat.rgb * pow(max(dot(normalize(reflect(normalize(light_dir), normal)), viewDir), 0), specularMat.a) * vec3(texture(specularMap, uv2	.xy));
-						#else
-							specular = specularMat.rgb * pow(max(dot(normalize(reflect(normalize(light_dir), normal)), viewDir), 0), specularMat.a) * vec3(texture(specularMap, oUv0.xy));
-						#endif 
+						specular = specularMat.rgb * pow(max(dot(normalize(reflect(light_dir, normal)), viewDir), 0), specularMat.a) * vec3(texture(specularMap, oUv0.xy));
                     #else
-                        specular = specularMat.rgb * pow(max(dot(normalize(reflect(normalize(light_dir), normal)), viewDir), 0), specularMat.a);
+                        specular = specularMat.rgb * pow(max(dot(normalize(reflect(light_dir, normal)), viewDir), 0), specularMat.a);
                     #endif
                 #endif
-                result = result + attenuation * (ambient + diffuse + specular);
+                result = result + attenuation * (diffuse + specular) * diffuseColor.rgb;
+				//result = result + ambient + diffuse + specular;
+				//result = vec3(1.0, 0, 0);
             #endif
         }
         else if(light_type_array[i] == 2) //聚光灯
         {
-            vec3 light_dir = fragPos - light_position_array[i];
+            vec3 light_dir =  light_position_array[i] - fragPos;
 			float dis = length(light_dir);
-            float attenuation = 1/(light_attenuation_array[i].r +light_attenuation_array[i].g * dis + light_attenuation_array[i].b * dis * dis); 
-            float theta = dot(normalize(light_dir), normalize(light_direction_array[i]));
-            if(theta > spotlight_params_array[i])
+			light_dir = normalize(light_dir);
+            float attenuation = 1/(light_attenuation_array[i].g +light_attenuation_array[i].b * dis + light_attenuation_array[i].a * dis * dis); 
+            float theta = dot(light_dir, normalize(light_direction_array[i]));
+            if(theta < spotlight_params_array[i][0])
             {
-                vec3 ambient = AmbientColor.rgb * ambientMat.rgb * diffuseColor.rgb;
-                vec3 diffuse = light_diffuse_array[i].rgb * diffuseMat.rgb * max(dot(normalize(-light_dir), normal), 0) * diffuseColor.rgb;
+                vec3 diffuse = light_diffuse_array[i].rgb * diffuseMat.rgb * max(dot(light_dir, normal), 0.0f);
                 #ifdef LAMBERT
-                     result = result + attenuation * (ambient + diffuse);
+					float intensity=clamp((spotlight_params_array[i][1] - theta)/(spotlight_params_array[i][1]-spotlight_params_array[i][0]),0.0,1.0); //我们不希望强度值在0与1之外
+                     result = result + attenuation * intensity * diffuse * diffuseColor.rgb;
                 #else
                     vec3 specular;
                     #ifdef BLINN_PHONE
                         #ifdef SPECULAR_MAP
-                            #ifdef NORMAL_HEIGHTMAP
-								specular = specularMat.rgb * pow(max(dot(normalize(normalize(-light_direction_array[i]) + viewDir), normal), 0), shiness) * vec3(texture(specularMap, uv2.xy));
-							#else
-								specular = specularMat.rgb * pow(max(dot(normalize(normalize(-light_direction_array[i]) + viewDir), normal), 0), shiness) * vec3(texture(specularMap, oUv0.xy));
-							#endif    
+							specular = specularMat.rgb * pow(max(dot(normalize(light_dir + viewDir), normal), 0.0f), specularMat.a) * vec3(texture(specularMap, oUv0.xy));  
 						#else
-                            specular= specularMat.rgb * pow(max(dot(normalize(normalize(-light_dir) + normalize(viewDir)), normal), 0), specularMat.a);
+                            specular= specularMat.rgb * pow(max(dot(normalize(light_dir + viewDir), normal), 0.0f), specularMat.a);
                         #endif
                     #else
                         #ifdef SPECULAR_MAP
-							#ifdef NORMAL_HEIGHTMAP
-								specular= specularMat.rgb * pow(max(dot(normalize(reflect(normalize(light_dir), normal)), viewDir), 0), specularMat.a) * vec3(texture(specularMap, uv2.xy));
-							#else
-								specular= specularMat.rgb * pow(max(dot(normalize(reflect(normalize(light_dir), normal)), viewDir), 0), specularMat.a) * vec3(texture(specularMap, oUv0.xy));
-							#endif    
+							pecular= specularMat.rgb * pow(max(dot(normalize(reflect(light_dir, normal)), viewDir), 0.0f), specularMat.a) * vec3(texture(specularMap, oUv0.xy));   
                         #else
-                            specular= specularMat.rgb * pow(max(dot(normalize(reflect(normalize(light_dir), normal)), viewDir), 0), specularMat.a);
+                            specular= specularMat.rgb * pow(max(dot(normalize(reflect(light_dir, normal)), viewDir), 0.0f), specularMat.a);
                         #endif
                     #endif
-                    result = result + attenuation * (ambient + diffuse + specular);
+					float intensity=clamp((spotlight_params_array[i][1] - theta)/(spotlight_params_array[i][1]-spotlight_params_array[i][0]),0.0,1.0); //我们不希望强度值在0与1之外
+                   result = result + attenuation * intensity * (diffuse + specular) * diffuseColor.rgb;
                 #endif
             }
         }
      }
 	 
-#ifdef RECEIVE_SHADOW_PSSM
-	float shadowing = 1.0f;
-	//float4 splitColour;
-	if (zPosition <= pssmSplitPoints.y)
-	{
-		//splitColour = float4(0.5, 0, 0, 1);
-		shadowing = shadowPCF(shadowMap0, oLightPosition0, invShadowMapSize0.xy);
-	}
-	else if (zPosition <= pssmSplitPoints.z)
-	{
-		//splitColour = float4(0, 0.5, 0, 1);
-		shadowing = shadowPCF(shadowMap1, oLightPosition1, invShadowMapSize1.xy);	
-	}
-	if(shadowing < 0.25)
-	{
-		shadowing = 0.0;
-	}
-	else
-	{
-		shadowing = 1.0;
-	}
 
-    result = result * shadowing;
-#endif
 
 //#ifdef FOG_LINEAR
 	////float fog = ( fogParams.z - eyeDistance ) * fogParams.w; // / ( fogParams.z - fogParams.y );
@@ -358,9 +306,7 @@ void main()
 	 
 	//FragColor = vec4(1.0,0.0,0.0, 1.0);
 	FragColor = vec4(result, diffuseColor.a);
-    if (light_count == 0) { FragColor = emissiveMat * diffuseColor; }
 }
  
-
 
 #endif
